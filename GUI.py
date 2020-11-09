8c7f403e8dcdf07c43e36045ae526576ee62008f
tieje <toj320@gmail.com>
2020-11-08 23:52:39 -0500

    pivoting project to be simpler. I learned a lot in the process and that's what matters. Removing the manual review. Focusing on email exporting to a file.

--- GUI.py
+++ GUI.py
@@ -31,9 +31,9 @@ Modules that need to be built:
 - "Begin Scraping" button
     + created button
     - calls on the web scraping module
-        - will print which business is being searched and the URLs that are being scraped
-        - will input the scraped data into the excel sheet
-        - I'll need to run this as a subprocess so that the user can maintain control of the gui.
+        + will print which business is being searched and the URLs that are being scraped
+        + will input the scraped data into the excel sheet
+        + I'll need to run this as a subprocess so that the user can maintain control of the gui.
     - 
 - "Stop Scraping" button
     + created button
@@ -50,7 +50,7 @@ Modules that need to be built:
 End-User experience
 x checkmark that says show "help" text
 + feedback window
-- choose theme
++ choose theme
 - choose font
 - organize logically
     - organize in steps
@@ -71,14 +71,179 @@ The right is going to be reserved for the feedback and status conditions.
 2. Detect Excel sheets button
 3. Choose which excel sheet to use.
 4. Input text about which column to perform search on.
+
+                                [
+                                    sg.Text(
+                                        'Number of Top Results to Scrape (Max is "5")',
+                                        pad=pad_text_association,
+                                    ),
+                                ],
+                                [
+                                    sg.InputText(
+                                        default_text="1",
+                                        do_not_clear=True,
+                                        size=(2, 1),
+                                        justification="center",
+                                        key=top_results_inputtext,
+                                    )
+                                ],
+
 """
 
 import PySimpleGUI as sg
 import detect_excels as des
-import web_scraper as ws
-import subprocess
+import multiprocessing as mp
+import re
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import logging
+import xlwings as xw
+import time
 
-sg.theme("DarkAmber")
+""" Values
+{
+    "skip review checkbox": False,
+    "emails checkbox": True,
+    "phone numbers checkbox": True,
+    "contact urls checkbox": True,
+    "excel_chosen_listbox": [],
+    "excel column": "A",
+    "addendum": "",
+}
+"""
+# message receiver from web scraper
+
+
+def start_comm():
+    print("entered comms")
+    global begin_scrape_child
+    begin_scrape_child.send("Manual Review")
+    print("parent reception " + str(begin_scrape_parent.poll()))
+    while True:
+        if begin_scrape_child.poll():
+            break
+
+
+def contact_scraper(**settings):
+    global begin_scrape_child
+    print("reached contact scraper")
+    # logging what actually happens
+    logging.basicConfig(level=logging.INFO)
+    logging.debug("Debugging is on")
+    print(settings["emails checkbox"])
+
+    # initializing chromedriver
+    chromedriver_args = [
+        "--window-size=1920x1080",
+        "start-maximized",
+        "disable-popup-blocking",
+    ]
+
+    chrome_options = Options()
+    chrome_options.headless = False
+    for i in chromedriver_args:
+        chrome_options.add_argument(i)
+    contact_list = [
+        "contact us",
+        "Contact Us",
+        "CONTACT US",
+        "contact",
+        "CONTACT",
+        "Contact",
+    ]
+
+    # initializing excel connection
+
+    wb = xw.books["".join(settings["excel_chosen_listbox"])].sheets[0]
+    # begin scraping on the second row
+    counter = 2
+    # establish the next three open columns. Assumes that the next three columns are open after the first blank
+    free_column = 65
+    cell_value = wb.range(chr(free_column) + "1").value
+    while bool(cell_value):
+        free_column += 1
+        cell_value = wb.range(chr(free_column) + "1").value
+    # Begin web scraping based on excel cell values in the chosen column
+    driver = webdriver.Chrome(options=chrome_options)
+    while bool(wb.range(settings["excel column"] + str(counter)).value):
+
+        # defining variable cells
+        email_cell = wb.range(chr(free_column) + str(counter))
+        phone_cell = wb.range(chr(free_column) + str(counter + 1))
+        contact_url_cell = wb.range(chr(free_column) + str(counter + 2))
+        # stop scraping mechanism #1
+
+        if begin_scrape_child.poll():
+            break
+
+        # connecting search engine
+
+        driver.get("https://duckduckgo.com/")
+        search_box = driver.find_element_by_xpath(
+            '//*[@id="search_form_input_homepage"]'
+        )
+
+        search_box.send_keys(wb.range(settings["excel column"] + str(counter)).value)
+
+        # main standard scraping procedure.
+        search_button = driver.find_element_by_xpath(
+            '//*[@id="search_button_homepage"]'
+        )
+        search_button.click()
+        first_result = driver.find_element_by_xpath('//*[@id="r1-0"]')
+        first_result.click()
+
+        for i in contact_list:
+            try:
+                print(i)
+                contact_us = driver.find_element_by_xpath('//*[text()="%s"]' % i)
+                print(contact_us)
+                contact_us.click()
+            except:
+                try:
+                    contact_us = driver.find_element_by_link_text(i)
+                    contact_us.click()
+                except:
+                    pass
+                contact_us = 0
+            if contact_us != 0:
+                break
+
+        if settings["phone numbers checkbox"]:
+            phone_list = re.findall(
+                "(\d{3}[-\.\s]\d{3}[-\.\s]\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]\d{4})",
+                driver.page_source,
+            )
+            phone_cell.value = "\n".join(set([d.lower() for d in phone_list]))
+        if settings["emails checkbox"]:
+            email_list = re.findall(
+                "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", driver.page_source
+            )
+            email_cell.value = "\n".join(set([d.lower() for d in email_list]))
+        if settings["contact urls checkbox"]:
+            contact_url = driver.current_url
+            contact_url_cell = "\n".join(contact_url)
+
+        if settings["skip review checkbox"] == False:
+            if phone_list == [] and email_list == []:
+                print("entered review")
+                start_comm()
+                receipt = begin_scrape_child.recv()
+                print("The receipt is: " + str(receipt))
+                if receipt:
+                    pass
+                else:
+                    break
+
+        # stop scraping mechanism #2
+        if begin_scrape_child.poll():
+            break
+
+        print(counter)
+        counter += 1
+    begin_scrape_child.send(True)
+    return None
+
 
 pad_text_association = ((0, 0), (10, 5))
 pad_button = ((0, 0), (10, 10))
@@ -97,24 +262,18 @@ phone_numbers_checkbox = "phone numbers checkbox"
 contact_urls_checkbox = "contact urls checkbox"
 excel_chosen_listbox = "excel_chosen_listbox"
 excel_column_inputtext = "excel column"
-top_results_inputtext = "top results"
+# top_results_inputtext = "top results"
 addendum_inputtext = "addendum"
+choose_this_file = "chosen file"
 
 # Initializing variables
 detected_excels = des.DetectExcels.detect()
-"""
-sanitizing functions
-case_not_integer = "An integer was not entered. Please Enter an integer."
-def sanitize_integer_input(tester, max_numb):
-    while tester.isdigit() == False:
-        print("\n" + case_1_not_integer + "\n")
-        tester = input(">")
-    while int(tester) > max_numb or int(tester) < 1:
-        print("\n" + case_2_not_acceptable + "\n")
-        tester = input(">")
-    return tester
 
-"""
+# Initializing pipes
+
+begin_scrape_parent, begin_scrape_child = mp.Pipe()
+
+sg.theme("DarkAmber")
 
 layout = [
     [
@@ -142,7 +301,9 @@ layout = [
                                     "Emails", default=True, key=emails_checkbox
                                 ),
                                 sg.Checkbox(
-                                    "Phone #s", default=True, key=phone_numbers_checkbox
+                                    "Phone #s",
+                                    default=True,
+                                    key=phone_numbers_checkbox,
                                 ),
                                 sg.Checkbox(
                                     "Contact URLs",
@@ -158,6 +319,12 @@ layout = [
                                 )
                             ],
                             [
+                                sg.Text(
+                                    "Pick a document to scrape",
+                                    pad=pad_text_association,
+                                )
+                            ],
+                            [
                                 sg.Listbox(
                                     values=detected_excels,
                                     auto_size_text=True,
@@ -166,9 +333,16 @@ layout = [
                                 )
                             ],
                             [
+                                sg.Button(
+                                    button_text="Choose This File",
+                                    pad=pad_button,
+                                    key=choose_this_file,
+                                )
+                            ],
+                            [
                                 sg.Text(
                                     "Excel Column to Scrape Data",
-                                    pad=((0, 0), (10, 5)),
+                                    pad=pad_text_association,
                                 )
                             ],
                             [
@@ -178,21 +352,6 @@ layout = [
                                     size=(3, 1),
                                     justification="center",
                                     key=excel_column_inputtext,
-                                )
-                            ],
-                            [
-                                sg.Text(
-                                    'Number of Top Results to Scrape (Max is "5")',
-                                    pad=pad_text_association,
-                                ),
-                            ],
-                            [
-                                sg.InputText(
-                                    default_text="1",
-                                    do_not_clear=True,
-                                    size=(2, 1),
-                                    justification="center",
-                                    key=top_results_inputtext,
                                 )
                             ],
                             [
@@ -215,13 +374,14 @@ layout = [
                                     button_text="Begin Scraping",
                                     pad=pad_button,
                                     key=begin_button,
+                                    visible=False,
                                 ),
                             ],
                             [
                                 sg.Button(
                                     button_text="Stop All Scraping",
                                     pad=((0, 0), (62, 5)),
-                                    visible=True,
+                                    visible=False,
                                     key=stop_button,
                                 ),
                             ],
@@ -267,7 +427,7 @@ layout = [
                                 sg.Button(
                                     button_text="Continue Scraping",
                                     pad=pad_button,
-                                    visible=True,
+                                    visible=False,
                                     key=continue_button,
                                 ),
                             ],
@@ -289,7 +449,7 @@ window = sg.Window(
     # finalize=True,
 )
 """ Values
-{
+settings = {
     "skip review checkbox": False,
     "emails checkbox": True,
     "phone numbers checkbox": True,
@@ -301,23 +461,76 @@ window = sg.Window(
 }
 """
 
-while True:
-    event, values = window.read()
-    print("The event is: " + str(event))
-    print("The value is: " + str(values))
-    if event == sg.WINDOW_CLOSED or event == "Quit":
-        break
-    if event == detect_excel_button:
-        print("Triggerring excel detection function. ")
-        detected_excels = des.DetectExcels.detect()
-        print(detected_excels)
-        window[excel_chosen_listbox].update(values=detected_excels)
-    if event == begin_button:
-        print("Triggerring Begin Scraping function.")
 
-    if event == continue_button:
-        print("Triggerring Continue function.")
-        window[continue_button].update(visible=False)
-    if event == stop_button:
-        print("Triggerring Stop Scraping.")
-window.close()
+def communicado():
+    print("entered communicado")
+    global begin_scrape_parent
+    global begin_scrape_child
+    global window
+    print("parent is: " + str(begin_scrape_parent.poll()))
+    while True:
+        if begin_scrape_parent.poll():
+            scraper_message = begin_scrape_parent.recv()
+            if scraper_message == "Manual Review":
+                window[continue_button].update(visible=True)
+                print(
+                    "Scraping has been paused because no phone numbers or emails were scraped. Please manually review the website for contact forms. Press the continue button when ready."
+                )
+                break
+            if scraper_message == True:
+                print("Web Scraping Completed")
+                window[begin_button].update(visible=True)
+                window[stop_button].update(visible=False)
+                p2.join()
+                break
+        event, values = window.read()
+        print("The event is: " + str(event))
+        print("The value is: " + str(values))
+        if event == sg.WINDOW_CLOSED or event == "Quit":
+            break
+        if event == choose_this_file:
+            print("You have chosen " + "".join(values["excel_chosen_listbox"]))
+        if values["excel_chosen_listbox"] != []:
+            window[begin_button].update(visible=True)
+        if event == detect_excel_button:
+            print("Detecting open Excel docs")
+            detected_excels = des.DetectExcels.detect()
+            print(detected_excels)
+            window[excel_chosen_listbox].update(values=detected_excels)
+        if event == begin_button:
+            print("Scraping has begun. Do not close the Excel doc.")
+            begin_scraping = mp.Process(
+                target=contact_scraper,
+                kwargs=values,
+            )
+            begin_scraping.start()
+            window[begin_button].update(visible=False)
+            window[stop_button].update(visible=True)
+        if event == continue_button:
+            print("Continue Scraping")
+            begin_scrape_parent.send(True)
+            window[continue_button].update(visible=False)
+        if event == stop_button:
+            print("Stop All Scraping.")
+            begin_scrape_parent.send(False)
+            window[begin_button].update(visible=True)
+            window[stop_button].update(visible=False)
+            begin_scraping.join()
+
+
+if __name__ == "__main__":
+    communicado()
+    window.close()
+
+"""
+I'm going to give up on pipes. 
+The reason for pipe failure is probably because of the framework I'm using for the gui.
+Well I guess also the language to some extent.
+
+remove manual review. it's too difficult for a simple gui application because there needs to be a better controller and the gui needs to be more complex to handle something like pausing without crashing.
+It's supposed to be a simple lead generator anyways.
+The manual work can be done by hand if needed.
+If anything, I should focus on an email exporter because that's the main idea anyways.
+
+
+"""
--- web_scraper.py
+++ web_scraper.py
@@ -2,6 +2,10 @@ import re
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import sys
+import logging
+import xlwings as xw
+import time
+from gui import 
 
 """ Values
 {
@@ -11,14 +15,20 @@ import sys
     "contact urls checkbox": True,
     "excel_chosen_listbox": [],
     "excel column": "A",
-    "top results": "1",
     "addendum": "",
 }
 """
 
 
 class Scraper:
-    def contact_scraper():
+    def contact_scraper(conn, **settings):
+        print("reached contact scraper")
+        # logging what actually happens
+        logging.basicConfig(level=logging.INFO)
+        logging.debug("Debugging is on")
+        print(settings["emails checkbox"])
+
+        # initializing chromedriver
         chromedriver_args = [
             "--window-size=1920x1080",
             "start-maximized",
@@ -29,7 +39,6 @@ class Scraper:
         chrome_options.headless = False
         for i in chromedriver_args:
             chrome_options.add_argument(i)
-
         contact_list = [
             "contact us",
             "Contact Us",
@@ -39,51 +48,98 @@ class Scraper:
             "Contact",
         ]
 
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get("https://duckduckgo.com/")
-        search_box = driver.find_element_by_xpath(
-            '//*[@id="search_form_input_homepage"]'
-        )
-        search_box.send_keys("JEDER VALUATION CONSULTANTS INC connecticut")
-        search_button = driver.find_element_by_xpath(
-            '//*[@id="search_button_homepage"]'
-        )
-        search_button.click()
-        first_result = driver.find_element_by_xpath('//*[@id="r1-0"]')
-        first_result.click()
+        # initializing excel connection
 
-        for i in contact_list:
-            try:
-                print(i)
-                contact_us = driver.find_element_by_xpath('//*[text()="%s"]' % i)
-                print(contact_us)
-                contact_us.click()
-            except:
+        wb = xw.books["".join(settings["excel_chosen_listbox"])].sheets[0]
+        # begin scraping on the second row
+        counter = 2
+        # establish the next three open columns. Assumes that the next three columns are open after the first blank
+        free_column = 65
+        cell_value = wb.range(chr(free_column) + "1").value
+        while bool(cell_value):
+            free_column += 1
+            cell_value = wb.range(chr(free_column) + "1").value
+        # Begin web scraping based on excel cell values in the chosen column
+        driver = webdriver.Chrome(options=chrome_options)
+        while bool(wb.range(settings["excel column"] + str(counter)).value):
+
+            # defining variable cells
+            email_cell = wb.range(chr(free_column) + str(counter))
+            phone_cell = wb.range(chr(free_column) + str(counter + 1))
+            contact_url_cell = wb.range(chr(free_column) + str(counter + 2))
+            # stop scraping mechanism #1
+
+            if conn.poll():
+                break
+
+            # connecting search engine
+
+            driver.get("https://duckduckgo.com/")
+            search_box = driver.find_element_by_xpath(
+                '//*[@id="search_form_input_homepage"]'
+            )
+
+            search_box.send_keys(
+                wb.range(settings["excel column"] + str(counter)).value
+            )
+
+            # main standard scraping procedure.
+            search_button = driver.find_element_by_xpath(
+                '//*[@id="search_button_homepage"]'
+            )
+            search_button.click()
+            first_result = driver.find_element_by_xpath('//*[@id="r1-0"]')
+            first_result.click()
+
+            for i in contact_list:
                 try:
-                    contact_us = driver.find_element_by_link_text(i)
+                    print(i)
+                    contact_us = driver.find_element_by_xpath('//*[text()="%s"]' % i)
+                    print(contact_us)
                     contact_us.click()
                 except:
-                    pass
-                contact_us = 0
-            if contact_us != 0:
-                break
-        phone_list = re.findall(
-            "(\d{3}[-\.\s]\d{3}[-\.\s]\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]\d{4})",
-            driver.page_source,
-        )
-        email_list = re.findall(
-            "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", driver.page_source
-        )
-        contact_url = driver.current_url
+                    try:
+                        contact_us = driver.find_element_by_link_text(i)
+                        contact_us.click()
+                    except:
+                        pass
+                    contact_us = 0
+                if contact_us != 0:
+                    break
 
-        if phone_list == [] and email_list == []:
-            print(
-                "The script could not scrape anything. Manual website review is required. E.g. there might be a formal request form that needs to be filled out manually.\n"
-            )
-            print(
-                "\nTo continue running the script after manual review, type 'c' and press Enter. To stop the script, press Enter"
-            )
-        return True
-        print(phone_list)
-        print(email_list)
-        print(contact_url)
+            if settings["phone numbers checkbox"]:
+                phone_list = re.findall(
+                    "(\d{3}[-\.\s]\d{3}[-\.\s]\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]\d{4})",
+                    driver.page_source,
+                )
+                phone_cell.value = "\n".join(set([d.lower() for d in phone_list]))
+            if settings["emails checkbox"]:
+                email_list = re.findall(
+                    "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+", driver.page_source
+                )
+                email_cell.value = "\n".join(set([d.lower() for d in email_list]))
+            if settings["contact urls checkbox"]:
+                contact_url = driver.current_url
+                contact_url_cell = "\n".join(contact_url)
+
+            if settings["skip review checkbox"] == False:
+                if phone_list == [] and email_list == []:
+                    print("entered review")
+                    conn.send("Manual Review")
+                    while conn.poll() == False:
+                        time.sleep(0.1)
+                    receipt = conn.recv()
+                    print("The receipt is: " + receipt)
+                    if receipt:
+                        pass
+                    else:
+                        break
+
+            # stop scraping mechanism #2
+            if conn.poll():
+                break
+
+            print(counter)
+            counter += 1
+        conn.send(True)
+        return None
